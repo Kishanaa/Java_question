86834dabc819a1d652f067c1cae01ce9ba6572e9
Kishan Shrivastava <newkishan13@gmail.com>
2024-04-07 23:59:24 +0530

    first commit

--- src/small_num_find.java
+++ src/small_num_find.java
@@ -1,2 +1,19 @@
+
 public class small_num_find {
+    static int small(int[] num){
+
+        int smallest=num[1];
+
+        for (int i=0;i< num.length;i++){
+            if (smallest>num[i]){
+                smallest=num[i];
+            }
+        }
+        return smallest;
+    }
+    public static void main(String[] args) {
+
+        int[] num={2,5,3,7,9};
+        System.out.println(small(num));
+    }
 }
